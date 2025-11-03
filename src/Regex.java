9ce3dd5e2f410a10924ee3bbf9cc9e9948d47f68
Josip <ujdur.jo@gmail.com>
2025-11-03 21:05:24 +0100

    Zadatak 2. (regex) 3.11.2025. novi commit

--- src/Regex.java
+++ src/Regex.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
 
-
-/*Napišite program u Javi koji će učitati niz znakova (string) sa standardnog ulaza i okrenuti redoslijed riječi u unesenom stringu. Prikazati transformirani string s obrnutim redoslijedom riječi.
+/*Napišite program u Javi koji će učitati niz znakova (string) sa standardnog ulaza i okrenuti redoslijed riječi u unesenom stringu.
+Prikazati transformirani string s obrnutim redoslijedom riječi.
 Primjer ulaza: Dobar dan svima
 Primjer izlaza: svima dan Dobar*/
 public class Regex {
     public static void main(String[] args) {
-        String text = " ";
-        String[] characters = text.split(" ");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Unesite željenu rečenicu: ");
+        String text = sc.nextLine();
+
+       // String text = "Danas je    novi aa daaan";
+        String[] characters = text.split("\\s");
 //        String[] characters = text.split("[\\s]");
 
         for (int i = characters.length - 1; i >= 0; i--) {
